6364ccce7747077cec3cec85abc1290f5c191184
AndrewMac34 <ajmvgd@gmail.com>
2020-06-02 13:03:57 -0500

    creating app lite_app.py

--- city_scrape.py
+++ city_scrape.py
@@ -5,8 +5,16 @@ import gmaps
 import pandas as pd
 # from config import api_key gkey
 from pprint import pprint
+import sqlite3 
+from flask import g
 
-# read csv file
-file = "./data/charity_data.csv"
-cities_data_df = pd.read_csv(file)
-cities_data_df.count()
+database = './data/Charity.sqlite'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(database)
+    return db
+@app.teardown_appcontext
+def close_connection(exception)
+def
