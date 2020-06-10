d46e301da5efaa3bb669959ca905062b2d833b90
AndrewMac34 <ajmvgd@gmail.com>
2020-06-10 16:50:10 -0500

    working on new app

--- app.py
+++ app.py
@@ -1,102 +1,77 @@
-import sqlite3
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func 
-from flask import g, request, jsonify, after_this_request, render_template
-from flask import Flask
-import numpy as np
-from datetime import datetime, timedelta
-import pandas as pd
+# import necessary libraries
+import os
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
 
 from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
 
-#charity_data=pd.read_sql("select * from charity",conn)
-database = './data/Charity_new.sqlite'
+# Remove tracking modifications
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app = Flask(__name__)
+db = SQLAlchemy(app)
 
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(database)
-    db.row_factory = sqlite3.Row
-
-    return db
-
-def query_db(query, args=(), one=False):
-    cur = get_db().execute(query, args)
-    rv = cur.fetchall()
-    cur.close()
-    return (rv[0] if rv else None) if one else rv
+from .models import Coord,Charity
 
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-@app.route('/paras')
-def paras():
-    parNames=['total_contributions','admin_expenses','leader_compensation','program_expenses','fundraising_expenses']
-    return jsonify(parNames)
-
-@app.route('/metrics')
-def metrics():
-    
-    metrics_data = []
-    columns = ['charity_name', 'city', 'state_abbr', 'organization_type', 'overall_score', 'administrative_expenses', 'compensation_leader_compensation', 'total_contributions', 'charity_id']
-    #metric = query_db('select charity_name, city, state_abbr, organization_type, overall_score, administrative_expenses, compensation_leader_compensation, total_contributions, charity_id from Charity order by charity_name')   
-    for metric in query_db('select charity_name, city, state_abbr, organization_type, overall_score, administrative_expenses, compensation_leader_compensation, total_contributions, charity_id from Charity order by charity_name'):
-    
-    
-        metrics_data.append(dict(zip(columns, metric)))
-
-    return jsonify(metrics_data)
-
-@app.route('/ranking')
-def ranking():
-    #cur = get_db().cursor()
-    metrics_data = []
-    columns = ['charity_name', 'city', 'state_abbr', 'organization_type', 'overall_score', 'administrative_expenses', 'compensation_leader_compensation', 'total_contributions', 'fundraising_expenses','program_expenses','charity_id']
-    #metric = query_db('select charity_name, city, state_abbr, organization_type, overall_score, administrative_expenses, compensation_leader_compensation, total_contributions, charity_id from Charity order by charity_name')
-    for metric in query_db('select charity_name, city, state_abbr, organization_type, overall_score, administrative_expenses, compensation_leader_compensation, total_contributions, program_expenses, fundraising_expenses, charity_id from Charity order by charity_name'):
-        metrics_data.append(dict(zip(columns, metric)))
-    return jsonify(metrics_data)
-
-@app.route('/names')
-def org():
-    orgNames = ['International', 'Human Services', 'Environment', 'Education',
-       'Human and Civil Rights', 'Health', 'Animals',
-       'Community Development', 'Religion',
-       'Arts, Culture, Humanities', 'Research and Public Policy']
-
-    return jsonify(orgNames)
-
-@app.route('/location')
-def locs():
-    
-    locations_data= []
-    columns = ['charity_name', 'lat', 'lng', 'city']
-    for location in query_db('select charity_name, lat, lng, city from Charity order by charity_name'):
-        locations_data.append(dict(zip(columns, location)))
-        
-
-    return jsonify(locations_data)
-
-
+# create route that renders index.html template
 @app.route("/")
-def index():
-    """Return the homepage."""
+def home():
     return render_template("index.html")
+
+
+# # Query the database and send the jsonified results
+# @app.route("/send", methods=["GET", "POST"])
+# def send():
+#     if request.method == "POST":
+#         name = request.form["petName"]
+#         lat = request.form["petLat"]
+#         lon = request.form["petLon"]
+
+#         pet = Pet(name=name, lat=lat, lon=lon)
+#         db.session.add(pet)
+#         db.session.commit()
+#         return redirect("/", code=302)
+
+#     return render_template("form.html")
+
+
+@app.route("/location")
+def locs():
+    results = db.session.query(Charity.charity_name, Coord.lat, Coord.lng, Coord_city).all()
+
+    charity_name = [result[0] for result in results]
+    lat = [result[1] for result in results]
+    lng = [result[2] for result in results]
+    city = [result[3] for result in results]
+    pet_data = [{
+        "charity_name": charity_name,
+        "lat": lat,
+        "lng": lng,
+        "city": city
+    }]
+
+    return jsonify(pet_data)
+
+
 
 @app.route("/metricspage")
 def metrics_page():
     """Metrics Page"""
     return render_template("metrics.html")
-
 
 
 @app.route("/map")
@@ -110,7 +85,5 @@ def rank():
     return render_template("rankings.html")
 
 
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
--- models.py
+++ models.py
@@ -1,13 +1,44 @@
 from .app import db
 
 
-class Pet(db.Model):
-    __tablename__ = 'pets'
+class Charity(db.Model):
+    __tablename__ = 'charities'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    lat = db.Column(db.Float)
-    lon = db.Column(db.Float)
+    charity_id = db.Column(db.Integer, primary_key=True)
+    accountability_score = db.Column(db.Float)
+    administrative_expenses = db.Column(db.Float)
+    charity_name = db.Column(db.String(64))
+    charity_url = db.Column(db.String(64))
+    city = db.Column(db.String(64))
+    cn_advisory = db.Column(db.String(64))
+    compensation_leader_compensation = db.Column(db.Float)
+    compensation_leader_expense_percent = db.Column(db.Float)
+    compensation_leader_title = db.Column(db.String(64))
+    excess_or_deficit_for_year = db.Column(db.Float)
+    financial_score = db.Column(db.Float)
+    fundraising_expenses = db.Column(db.Float)
+    net_assets = db.Column(db.Float)
+    organization_type = db.Column(db.String(64))
+    other_revenue = db.Column(db.Float) 
+    overall_score = db.Column(db.Float)
+    payments_to_affiliates = db.Column(db.Float)
+    program_expenses = db.Column(db.Float)
+    state_abbr = db.Column(db.String(64))
+    total_contributions = db.Column(db.Float)
+
 
     def __repr__(self):
-        return '<Pet %r>' % (self.name)
+        return '<Charity %r>' % (self.name)
+
+
+class Charity(db.Model):
+    __tablename__ = 'city_coord'
+
+    charity_id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(64))
+    state_abbr = db.Column(db.String(64))
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Charity %r>' % (self.name)
